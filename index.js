f2e22a46bd580b061b5b53384cfe46748cbd8965
herson <hersonx@gmail.com>
2023-05-08 22:37:28 -0600

    agregando mas enventos a websocket server

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ import { fileURLToPath } from "url";
 import { PORT } from "./config.js";
 import cors from "cors";
 
+var info = {
+  "mensaje": {},
+  "contador": 0,
+  "ledState": false,
+  "potVal": 0
+};
 
-var contador = 0;
 
 // Initializations
 const app = express();
@@ -29,11 +34,26 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(join(__dirname, "../client/build")));
 
 io.on("connection", (socket) => {
-  console.log("un usuario conectado.");
-  socket.on("message", (data) => {
-    socket.emit('respuesta', `${contador} : ${data}`);
-    contador++;
+
+  socket.on("init", (data) => {
+    socket.broadcast.emit('init', info);
   });
+  
+  socket.on("mensaje", (data) => {
+    info.mensaje = JSON.parse(data);
+    socket.broadcast.emit('mensaje', data);
+  });
+
+  socket.on("ledStatus", (data) => {
+    info.ledState = Boolean(data);
+    socket.broadcast.emit('ledStatus', data);
+  });
+
+  socket.on("potVal", (data) => {
+    info.potVal = Number(data);
+    socket.broadcast.emit('potVal', data);
+  });
+
 });
 
 server.listen(PORT);
